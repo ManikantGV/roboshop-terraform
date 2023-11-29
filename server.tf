a1540fb43e698568ab7f89fdedaa447151d3f8e1
Manikanta <manikanta.guntika@gmail.com>
2023-11-29 07:55:09 +0530

    terraform remote-exec logic

--- server.tf
+++ server.tf
@@ -10,6 +10,23 @@ resource "aws_instance" "instance" {
   tags = {
     Name = each.value["name"]
   }
+
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = self.private_ip
+    }
+
+    inline= [
+      "rm -f Roboshop",
+      "git clone https://github.com/ManikantGV/Roboshop",
+      "cd Roboshop",
+      "sudo bash ${each.value["name"]}.sh"
+    ]
+
+  }
 }
 
 resource "aws_route53_record" "frontend" {
@@ -21,119 +38,3 @@ resource "aws_route53_record" "frontend" {
   records = [aws_instance.instance[each.value["name"]].private_ip]
 }
 
-#
-#resource "aws_instance" "frontend" {
-#  ami           = data.aws_ami.centos.image_id
-#  instance_type = var.instance_type
-#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-#
-#  tags = {
-#    Name = "frontend"
-#  }
-#}
-#
-#resource "aws_route53_record" "frontend" {
-#  zone_id = "Z08997022LLSW1K3YTGW4"
-#  name    = "frontend.guntikadevops.online"
-#  type    = "A"
-#  ttl     = 300
-#  records = [aws_instance.frontend.private_ip]
-#}
-#
-#resource "aws_instance" "mongo" {
-#  ami           = data.aws_ami.centos.image_id
-#  instance_type = var.instance_type
-#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-#
-#  tags = {
-#    Name = "mongo"
-#  }
-#}
-#
-#resource "aws_route53_record" "mongo" {
-#  zone_id = "Z08997022LLSW1K3YTGW4"
-#  name    = "mongo.guntikadevops.online"
-#  type    = "A"
-#  ttl     = 300
-#  records = [aws_instance.mongo.private_ip]
-#}
-#
-#resource "aws_instance" "catalogue" {
-#  ami           = data.aws_ami.centos.image_id
-#  instance_type = var.instance_type
-#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-#
-#  tags = {
-#    Name = "catalogue"
-#  }
-#}
-#
-#resource "aws_instance" "cart" {
-#  ami           = data.aws_ami.centos.image_id
-#  instance_type = var.instance_type
-#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-#
-#  tags = {
-#    Name = "cart"
-#  }
-#}
-#
-#resource "aws_instance" "redis" {
-#  ami           = data.aws_ami.centos.image_id
-#  instance_type = var.instance_type
-#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-#
-#  tags = {
-#    Name = "redis"
-#  }
-#}
-#
-#resource "aws_instance" "user" {
-#  ami           = data.aws_ami.centos.image_id
-#  instance_type = var.instance_type
-#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-#
-#  tags = {
-#    Name = "user"
-#  }
-#}
-#
-#resource "aws_instance" "mysql" {
-#  ami           = data.aws_ami.centos.image_id
-#  instance_type = var.instance_type
-#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-#
-#  tags = {
-#    Name = "mysql"
-#  }
-#}
-#
-#resource "aws_instance" "shipping" {
-#  ami           = data.aws_ami.centos.image_id
-#  instance_type = var.instance_type
-#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-#
-#  tags = {
-#    Name = "shipping"
-#  }
-#}
-#
-#resource "aws_instance" "rabbitmq" {
-#  ami           = data.aws_ami.centos.image_id
-#  instance_type = var.instance_type
-#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-#
-#  tags = {
-#    Name = "rabbitmq"
-#  }
-#}
-#
-#resource "aws_instance" "payment" {
-#  ami           = data.aws_ami.centos.image_id
-#  instance_type = var.instance_type
-#  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-#
-#  tags = {
-#    Name = "payment"
-#  }
-#}
