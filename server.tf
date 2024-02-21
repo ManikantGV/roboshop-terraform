800a03a5a1e5114d2fb09e31ec7dfa4ac647ff01
Manikanta <manikanta.guntika@gmail.com>
2024-02-21 05:51:01 +0530

    Added all components

--- server.tf
+++ server.tf
@@ -1,46 +1,9 @@
-
-resource "aws_instance" "instance" {
-  #count = length(var.components)
+module "servers" {
   for_each = var.components
-  ami           = data.aws_ami.centos.image_id
+  source = "./module"
+  component_name = each.value["name"]
+ # components = ""
+  env = var.env
   instance_type = each.value["instance_type"]
-  vpc_security_group_ids = [ data.aws_security_group.allow-all.id ]
-
-  tags = {
-    Name = each.value["name"]
-  }
-
- }
-
-resource "null_resource" "provisioner" {
-
-  depends_on = [aws_instance.instance,aws_route53_record.records]
-
-  for_each = var.components
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = aws_instance.instance[each.value["name"]].private_ip
-    }
-
-    inline= [
-      "rm -rf Roboshop",
-      "git clone https://github.com/ManikantGV/Roboshop",
-      "cd Roboshop",
-      "sudo bash ${each.value["name"]}.sh ${lookup(each.value,"password","null")}"
-    ]
-
-  }
+  password = lookup(each.value,"password","null" )
 }
-
-resource "aws_route53_record" "records" {
-  for_each = var.components
-  zone_id = "Z08997022LLSW1K3YTGW4"
-  name    = "${each.value["name"]}-dev.guntikadevops.online"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.instance[each.value["name"]].private_ip]
-}
-
