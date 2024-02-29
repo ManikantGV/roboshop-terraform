ecb1e105bd6bd4941d079eeeb2814ca18f1cd453
Manikanta <manikanta.guntika@gmail.com>
2024-02-29 07:20:05 +0530

    Added all components

--- module/locals.tf
+++ module/locals.tf
@@ -8,7 +8,7 @@ locals {
   ]
   app_commands=[
     "sudo labauto ansible",
-    "ansible-pull -i localhost, -U https://github.com/ManikantGV/roboshop-ansible.git roboshop.yml -e env=${var.env} -e rolename=${var.component_name}"
+    "ansible-pull -i localhost, -U https://github.com/ManikantGV/roboshop-ansible roboshop.yml -e env=${var.env} -e rolename=${var.component_name}"
 
   ]
 }
