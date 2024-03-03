a82dc56f3b2510985e78455dd09fe96612e08d9e
Manikanta <manikanta.guntika@gmail.com>
2024-03-03 17:35:24 +0530

    Added all components

--- server.tf
+++ server.tf
@@ -17,6 +17,6 @@ module "apps-servers" {
   env = var.env
   instance_type = each.value["instance_type"]
   password = lookup(each.value,"password","null" )
-  provisioner = false
+  provisioner = true
   app_type="app"
 }
